7c5aaa97877bf7d36884393690ffdebccaee7d07
Facundo Labriola <facundolabriola@hotmail.com>
2023-09-20 15:15:15 -0300

    proyecto listo para la entrega final

--- proyecto_js/main.js
+++ proyecto_js/main.js
@@ -2,7 +2,6 @@ let input = document.querySelector(".input");
 let submit = document.querySelector(".add");
 let tasksDiv = document.querySelector(".tasks");
 
-
 let arrayOfTasks = [];
 
 // compruebo si hay tareas en el local storage
@@ -11,17 +10,23 @@ if (localStorage.getItem("tasks")) {
   getDataFromLocalStorage();
 }
 
-
-
-
 // agregar tarea
 submit.onclick = function () {
   if (input.value !== "") {
-    addTaskToArray(input.value); 
-    input.value = ""; 
+    addTaskToArray(input.value);
+    input.value = "";
   }
 };
 
+// uso de libreria sweet alert al agregar una tarea
+submit.addEventListener("click", () => {
+  Swal.fire({
+    title: "Genial!",
+    icon: "success",
+    text: "Has agregado una tarea",
+    confirmButtonText: "Ok!",
+  });
+});
 
 // eliminar tarea
 tasksDiv.addEventListener("click", (e) => {
@@ -30,10 +35,6 @@ tasksDiv.addEventListener("click", (e) => {
     e.target.parentElement.remove();
   }
 });
-
-
-
-
 
 // identifico tareas con date.now, guardo en el array vacio y los agrego a la pantalla y local storage
 function addTaskToArray(taskText) {
@@ -47,30 +48,38 @@ function addTaskToArray(taskText) {
   addDataToLocalStorageFrom(arrayOfTasks);
 }
 
-
 // por cada tarea creamos un div, boton de borrar y agregamos las tareas a los div
 function addElementsToPageFrom(arrayOfTasks) {
   tasksDiv.innerHTML = "";
-  
+
   arrayOfTasks.forEach((task) => {
     let div = document.createElement("div");
     div.className = "task";
     div.setAttribute("data-id", task.id);
     div.appendChild(document.createTextNode(task.title));
+
     let span = document.createElement("span");
     span.className = "del";
     span.appendChild(document.createTextNode("Borrar"));
     div.appendChild(span);
     tasksDiv.appendChild(div);
+
+    //uso de sweet alert al eliminar una tarea
+    span.addEventListener("click", () => {
+      Swal.fire({
+        title: "Una menos!",
+        icon: "info",
+        text: "Has eliminado una tarea",
+        confirmButtonText: "Ok!",
+      });
+    });
   });
 }
-
 
 // convierto el array en un string json y lo guardo en el local con la clave tasks
 function addDataToLocalStorageFrom(arrayOfTasks) {
   window.localStorage.setItem("tasks", JSON.stringify(arrayOfTasks));
 }
-
 
 // recupero los datos de las tareas, los parseo y los muestro en pantalla
 function getDataFromLocalStorage() {
@@ -80,7 +89,6 @@ function getDataFromLocalStorage() {
     addElementsToPageFrom(tasks);
   }
 }
-
 
 //elimino las tareas con su id
 function deleteTaskWith(taskId) {
